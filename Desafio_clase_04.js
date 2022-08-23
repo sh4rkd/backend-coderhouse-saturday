b22cf930f395679a91930b78943d054ffa45d65e
sh4rkd <20006132@es.uveg.edu.mx>
2022-08-23 17:30:45 -0600

    corrigiendo entrega

--- Desafio_clase_04.js
+++ Desafio_clase_04.js
@@ -27,98 +27,43 @@ class Contenedor {
         }
     }
 
-    save(producto) {
-        return new Promise((resolve, reject) => {
-            try {
-                let id = this.productos.length>0 ? this.productos[this.productos.length-1].id + 1 : 0;
-                producto.id = id;
-                this.productos.push(producto);
-                fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
-                resolve(id);
-            } catch (error) {
-                reject(error);
-            }
-        }).then(id => {
-            return id;
-        }).catch(error => {
-            return error;
-        });
+    save(producto) {        
+        let id = this.productos.length>0 ? this.productos[this.productos.length-1].id + 1 : 0;
+        producto.id = id;
+        this.productos.push(producto);
+        fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
+        return id;
     }
 
-    getById(id) {
-        return new Promise((resolve, reject) => {
-            try {
-                let producto = this.productos.find(producto => producto.id == id);
-                resolve(producto);
-            } catch (error) {
-                reject(error);
-            }
-        }).then(producto => {
-            return producto;
-        }).catch(error => {
-            return error;
-        });
+    getById(id) {        
+        let producto = this.productos.find(producto => producto.id == id);
+        return producto;
     }
 
     getAll() {
-        return new Promise((resolve, reject) => {
-            try {
-                resolve(this.productos);
-            } catch (error) {
-                reject(error);
-            }
-        }).then(productos => {
-            return productos;
-        }).catch(error => {
-            return error;
-        });
+        return this.productos;
     }
 
-    deleteById(id) {
-        return new Promise((resolve, reject) => {
-            try {
-                let producto = this.productos.find(producto => producto.id == id);
-                this.productos.splice(this.productos.indexOf(producto), 1);
-                fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        }).then(() => {
-            return;
-        }).catch(error => {
-            return error;
-        });
+    async deleteById(id) {        
+        let producto = this.productos.find(producto => producto.id == id);
+        this.productos.splice(this.productos.indexOf(producto), 1);
+        await fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
     }
 
-    deleteAll() {
-        return new Promise((resolve, reject) => {
-            try {
-                this.productos = [];
-                fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        }).then(() => {
-            return;
-        }).catch(error => {
-            return error;
-        });
+    async deleteAll() {
+        this.productos = [];
+        await fs.writeFileSync(this.nombreArchivo, JSON.stringify(this.productos));
     }
-}
-
-const printConsole = async (productos) => {
-    console.log(await productos);
 }
 
 const contenedor = new Contenedor('productos.txt');
 
-contenedor.save({                                                                                                                                                    
+console.log(contenedor.save({                                                                                                                                                    
       title: 'Escuadra',                                                                                                                                 
       price: 123.45,                                                                                                                                     
       thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                          
-});
+}))
+
 contenedor.save({                                                                                                                                                    
     title: 'Calculadora',                                                                                                                              
     price: 234.56,                                                                                                                                     
@@ -130,12 +75,12 @@ contenedor.save({
     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                    
 });
 
-printConsole(contenedor.getById(1));
-printConsole(contenedor.getById(2));
-printConsole(contenedor.getAll());
+console.log(contenedor.getById(1));
 
 contenedor.deleteById(1);
-printConsole(contenedor.getAll());
+
+console.log(contenedor.getAll());
 
 contenedor.deleteAll();
-printConsole(contenedor.getAll());
+
+console.log(contenedor.getAll());
