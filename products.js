0eafd01262dfd4fec0ba9ab5deb584a9307fa578
sh4rkd <20006132@es.uveg.edu.mx>
2022-09-03 11:05:28 -0600

    desafio 06

---
+++ products.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+//CRUD json products
+const products = require('./products.json');
+
+class Content{
+
+  create(title, price, thumbnail) {
+    const product = {
+      title,
+      price,
+      thumbnail,
+      id: products.length + 1
+    };
+      products.push(product);
+      fs.writeFileSync('./products.json', JSON.stringify(products));
+  }
+
+  read() {
+    return products;
+  }
+
+  update(id, product) {
+    products[id] = product;
+    fs.writeFileSync('./products.json', JSON.stringify(products));
+  }
+
+  deleteProduct(id) {
+    products.splice(id, 1);
+    fs.writeFileSync('./products.json', JSON.stringify(products));
+  }
+
+  readRandom() {
+    return products[Math.floor(Math.random() * products.length)];
+  }
+}
+
+module.exports = new Content();
+
